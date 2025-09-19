847d12498e9ff6cd8451d70ce633f275009b37fe
manoruo <manoruo@andrew.cmu.edu>
2025-09-18 22:37:20 -0400

    Part 1

---
+++ assignment1/experiments/360render.py
@@ -0,0 +1,104 @@
+import argparse
+
+import matplotlib.pyplot as plt
+import pytorch3d
+import torch
+
+from starter.utils import get_device, get_mesh_renderer, load_cow_mesh
+from pytorch3d.renderer.cameras import look_at_view_transform
+import numpy as np
+import imageio
+
+def render_cow(
+    cow_path="data/cow.obj", image_size=256, color=[0.7, 0.7, 1], device=None,
+):
+    # The device tells us whether we are rendering with GPU or CPU. The rendering will
+    # be *much* faster if you have a CUDA-enabled NVIDIA GPU. However, your code will
+    # still run fine on a CPU.
+    # The default is to run on CPU, so if you do not have a GPU, you do not need to
+    # worry about specifying the device in all of these functions.
+    if device is None:
+        print("yep")
+        device = get_device()
+
+    # Get the renderer.
+    renderer = get_mesh_renderer(image_size=image_size)
+
+    # Get the vertices, faces, and textures.
+    vertices, faces = load_cow_mesh(cow_path)
+    vertices = vertices.unsqueeze(0)  # (N_v, 3) -> (1, N_v, 3)
+    faces = faces.unsqueeze(0)  # (N_f, 3) -> (1, N_f, 3)
+    textures = torch.ones_like(vertices)  # (1, N_v, 3)
+    textures = textures * torch.tensor(color)  # (1, N_v, 3)
+    mesh = pytorch3d.structures.Meshes(
+        verts=vertices,
+        faces=faces,
+        textures=pytorch3d.renderer.TexturesVertex(textures),
+    )
+    mesh = mesh.to(device)
+
+    # Prepare the camera:
+    cameras = pytorch3d.renderer.FoVPerspectiveCameras(
+        R=torch.eye(3).unsqueeze(0), T=torch.tensor([[0, 0, 3]]), fov=60, device=device
+    )
+
+    # Place a point light in front of the cow.
+    lights = pytorch3d.renderer.PointLights(location=[[0, 0, -3]], device=device)
+
+    rend = renderer(mesh, cameras=cameras, lights=lights)
+    rend = rend.cpu().numpy()[0, ..., :3]  # (B, H, W, 4) -> (H, W, 3)
+    # The .cpu moves the tensor to GPU (if needed).
+    return rend
+
+
+def render_cow_360(cow_path="data/cow.obj", image_size=256, color=[0.7, 0.7, 1], device=None,
+):
+    if device is None:
+        device = get_device()
+    
+    images = []
+    # Get the renderer.
+    renderer = get_mesh_renderer(image_size=image_size)
+
+    # Get the vertices, faces, and textures.
+    vertices, faces = load_cow_mesh(cow_path)
+    
+    vertices = vertices.unsqueeze(0)  # (N_v, 3) -> (1, N_v, 3)
+    faces = faces.unsqueeze(0)  # (N_f, 3) -> (1, N_f, 3)
+    
+    textures = torch.ones_like(vertices)  # (1, N_v, 3)
+    textures = textures * torch.tensor(color)  # (1, N_v, 3)
+
+    mesh = pytorch3d.structures.Meshes(
+        verts=vertices,
+        faces=faces,
+        textures=pytorch3d.renderer.TexturesVertex(textures),
+    )
+    mesh = mesh.to(device)
+
+
+    # Place a point light in front of the cow.
+    lights = pytorch3d.renderer.PointLights(location=[[0, 0, -3]], device=device)
+
+    
+    for degree in range(360):
+        rot, t = look_at_view_transform(dist=3, elev=3, azim=degree, degrees=True, device=device)
+        cameras = pytorch3d.renderer.FoVPerspectiveCameras(
+            R=rot, T=t, fov=60, device=device
+        )
+        rend = renderer(mesh, cameras=cameras, lights=lights)
+        rend = rend.cpu().numpy()[0, ..., :3]
+        rend = (np.clip(rend, 0, 1) * 255).astype(np.uint8)
+        images.append(rend)
+    return images
+
+        
+
+
+
+if __name__ == "__main__":
+    
+    my_images = np.array(render_cow_360())  # List of images [(H, W, 3)]
+    duration = 1000 // 15  # Convert FPS (frames per second) to duration (ms per frame)
+
+    imageio.mimsave('my_gif.gif', my_images, duration=duration)
